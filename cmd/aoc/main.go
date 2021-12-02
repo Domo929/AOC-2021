153168e76dc1953bc5c9be254d74422b60f3547a
Dominic Cupo <Dominic.Cupo@Xandr.com>
2021-12-02 09:50:36 -0500

    Main cleanup and Day 2 addition

--- cmd/aoc/main.go
+++ cmd/aoc/main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"github.com/Domo929/AOC-2021/days/day_1"
+	"github.com/Domo929/AOC-2021/days/day_2"
+)
+
+var (
+	files = []string{
+		"inputs/day_1_ex.tsv",
+		"inputs/day_1_act.tsv",
+		"inputs/day_2_ex.tsv",
+		"inputs/day_2_act.tsv",
+	}
+
+	solvers = []func(string) error{
+		func(s string) error { return nil },
+		day_1.Solve,
+		day_2.Solve,
+	}
+
+	day = 2
 )
 
 func main() {
-	err := day_1.Solve("inputs/day_1_act.tsv")
+	err := solvers[day](files[3])
 	if err != nil {
 		panic(err)
 	}
